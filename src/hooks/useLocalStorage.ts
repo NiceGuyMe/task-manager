5f5808983af5fde45d32c17415ef25b59578e4c8
NiceGuyMe <loica.etudiant@gmail.com>
2023-06-15 11:39:40 +0300

    feat: localstorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,23 @@
-const useLocalStorage = () => {
+import { useState, useEffect } from "react";
 
-}
+const useLocalStorage = (key: any, initialValue: any) => {
+  const [value, setValue] = useState(() => {
+    const storedValue = localStorage.getItem(key);
+    if (storedValue) {
+      return JSON.parse(storedValue);
+    }
+    return initialValue;
+  });
 
-export {
-  useLocalStorage
-}
+  useEffect(() => {
+    if (value === null) {
+      localStorage.removeItem(key);
+    } else {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  }, [key, value]);
+
+  return { value, setValue };
+};
+
+export { useLocalStorage };
