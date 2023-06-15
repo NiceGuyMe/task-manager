c94f23bd0de30b2bce09d64754855e9071dc4439
NiceGuyMe <loica.etudiant@gmail.com>
2023-06-15 11:25:18 +0300

    fix: minutes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,7 @@ const calculateTimeDifference = (server: string, client: string) => {
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const seconds = Math.floor((diff % (1000 * 60 * 60)) / 1000);
-  const minutes =
-    Math.floor((diff % (1000 * 60 * 60)) / 1000);
+  const minutes = Math.floor(seconds / 60);
   return ` ${days} days, ${hours} hours, ${minutes} minutes,  ${seconds} seconds`;
 };
 
