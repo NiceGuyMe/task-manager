c41fbf8cecff01e9e739e6ff426c4ca39e5a1ce8
NiceGuyMe <loica.etudiant@gmail.com>
2023-06-15 11:33:00 +0300

    feat: zustand store

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,21 @@
-const useTaskManager = () => {
+import create from "zustand";
 
-}
+const useTaskManager = create((set) => ({
+  tasks: [],
+  searchTask: "",
+  setSearchTask: (searchTask: any) => set({ searchTask }),
+  addTask: (task: any) =>
+    set((state: { tasks: any }) => ({ tasks: [...state.tasks, task] })),
+  updateTask: (taskId: any, newTitle: any) =>
+    set((state: { tasks: any[] }) => ({
+      tasks: state.tasks.map((task: { id: any }) =>
+        task.id === taskId ? { ...task, title: newTitle } : task
+      ),
+    })),
+  deleteTask: (taskId: any) =>
+    set((state: { tasks: any[] }) => ({
+      tasks: state.tasks.filter((task: { id: any }) => task.id !== taskId),
+    })),
+}));
 
-export {
-  useTaskManager
-}
+export { useTaskManager };
